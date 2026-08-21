5af3081216ec345f9bea1ef0f368cf98e5373fc8
Doxpara1 <38714515+Doxpara1@users.noreply.github.com>
2026-08-20 21:12:55 -0400

    Update urls.py

--- sample_project/urls.py
+++ sample_project/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', lambda request: redirect('/api/'))
 ]
 
 if settings.DEBUG:
